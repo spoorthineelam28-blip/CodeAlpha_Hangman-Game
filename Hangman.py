d3eda4be25512349750dcd87c3523157b03ba3f3
spoorthineelam28-blip <spoorthineelam28@gmail.com>
2026-06-01 16:26:57 +0530

    Create Hangman.py

---
+++ Hangman.py
@@ -0,0 +1,48 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run something.")
+import random
+
+# Predefined list of words
+words = ["python", "apple", "school", "planet", "guitar"]
+
+# Select a random word
+word = random.choice(words)
+
+# Create hidden word display
+guessed_word = ["_"] * len(word)
+
+# Track guesses
+guessed_letters = []
+incorrect_guesses = 0
+max_incorrect = 6
+
+print("Welcome to Hangman!")
+
+while incorrect_guesses < max_incorrect and "_" in guessed_word:
+    print("\nWord:", " ".join(guessed_word))
+    print("Incorrect guesses left:", max_incorrect - incorrect_guesses)
+
+    guess = input("Enter a letter: ").lower()
+
+    # Check if already guessed
+    if guess in guessed_letters:
+        print("You already guessed that letter!")
+        continue
+
+    guessed_letters.append(guess)
+
+    # Check if letter is in the word
+    if guess in word:
+        print("Correct!")
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessed_word[i] = guess
+    else:
+        print("Wrong guess!")
+        incorrect_guesses += 1
+
+# End of game
+if "_" not in guessed_word:
+    print("\nCongratulations! You guessed the word:", word)
+else:
+    print("\nGame Over! The word was:", word)
